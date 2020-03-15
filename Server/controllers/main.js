d741eae09b61ebfda7a773197634e8ec6f1b428b
nickrally <nickrally2009@gmail.com>
2020-03-14 19:58:34 -0600

    support for comparison operators in query

--- Server/controllers/main.js
+++ Server/controllers/main.js
@@ -1,44 +1,29 @@
-/*   const getTableData = (req, res, db) => {
-    if (req.query){
-      db.select('*').from('repair').where(req.query)
-      .then(items => {
-        if(items.length){
-          console.log(`found ${items.length} items`)
-          res.json(items)
-        } else {
-          res.json({dataExists: 'false'})
-        }
-      })
-      .catch(err => res.status(400).json({dbError: 'db error'}))
-    }
-    else{
-      db.select('*').from('repair')
-      .then(items => {
-        if(items.length){
-          res.json(items)
-        } else {
-          res.json({dataExists: 'false'})
-        }
-      })
-      .catch(err => res.status(400).json({dbError: 'db error'}))
-    }
-  } */
 
+  
   const getTableData = (req, res, db) => {
     db.select('*').from('repair')
       .modify(function(queryBuilder){
-        console.log(req.query)
+        console.log('req.query before if:', req.query)
         if (Object.keys(req.query).length > 0) {
-          console.log("query params exist")
-          console.log(req.query)
-          let k = ''
-          let val = ''
-          for (const key in req.query) {
-            k = key
-            val = req.query[key]
-            console.log(`val: ${val}, key: ${k}`)
+          const keys = Object.keys(req.query)
+          const k = keys[0]
+          const re = /(?<left>\w+<|>)/ //<--- regex that identifies 'price<' type of pattern caused by gte; and lte; signs
+          const result = re.exec(k)
+          if(result && req.query[k]){
+            const left = k.slice(0,-1)
+            const operator = k.charAt(k.length-1) + '='
+            const right = req.query[k]
+            queryBuilder.where(left, operator, right)
+          } else {
+            const re2 = /(?<left>\w+)(?<operator>>|<)(?<right>\d+)/ //<--- regex that matches gt; and lt; signs
+            const result2 = re2.exec(k)
+            if(result2){
+              queryBuilder.where(result2.groups.left, result2.groups.operator, result2.groups.right)
+            } else {   //<--- default when query param is used with '=', e.g. 'price=0'
+              const val = req.query[k]
+              queryBuilder.where(k, val);
+            }
           }
-          queryBuilder.where(k, val);
         } 
       }).then(items => {
         console.log("outside of if")
@@ -58,43 +43,6 @@
       .then(items => {
         if(items.length){
           console.log(`found ${items.length} items`)
-          res.json(items)
-        } else {
-          res.json({dataExists: 'false'})
-        }
-      })
-      .catch(err => res.status(400).json({dbError: 'db error'}))
-  }
-
-  const queryByField = (req, res, db) => {
-    db.select('*').from('repair').where(req.query)
-      .then(items => {
-        if(items.length){
-          console.log(`found ${items.length} items`)
-          res.json(items)
-        } else {
-          res.json({dataExists: 'false'})
-        }
-      })
-      .catch(err => res.status(400).json({dbError: 'db error'}))
-  }
-
-  const queryTableDataByTag = (req, res, db, tag) => {
-    db.select('*').from('repair').where({tag:tag})
-      .then(items => {
-        if(items.length){
-          res.json(items)
-        } else {
-          res.json({dataExists: 'false'})
-        }
-      })
-      .catch(err => res.status(400).json({dbError: 'db error'}))
-  }
-
-  const queryTableDataByTerm = (req, res, db, searchTerm) => {
-    db.select('*').from('repair').where('repair.description', 'LIKE', `%${searchTerm}%`)
-      .then(items => {
-        if(items.length){
           res.json(items)
         } else {
           res.json({dataExists: 'false'})
